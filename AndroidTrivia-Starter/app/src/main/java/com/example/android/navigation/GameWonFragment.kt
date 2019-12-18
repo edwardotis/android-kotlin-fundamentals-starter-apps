9718333f76e14595434103f9126b2480d049bbba
Ed Johnson <ed@edwardotis.com>
2019-12-17 20:40:30 -0800

    up button

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -32,9 +32,9 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
-        binding.nextMatchButton.setOnClickListener({view : View ->
+        binding.nextMatchButton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
-        })
+        }
         return binding.root
     }
 }
