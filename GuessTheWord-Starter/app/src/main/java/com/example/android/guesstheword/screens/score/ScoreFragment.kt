79c592834cc9d93e004294dfff23bbd0eb3abc49
Ed Johnson <ed@edwardotis.com>
2020-01-09 15:14:13 -0800

    livedata in scorefrag

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
@@ -50,7 +51,10 @@ class ScoreFragment : Fragment() {
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
         viewModel = viewModelFactory.create(ScoreViewModel::class.java)
 //        val args = ScoreFragmentArgs.fromBundle(arguments!!)
-        binding.scoreText.text = viewModel.score.toString()
+//        binding.scoreText.text = viewModel.score.toString()
+        viewModel.score.observe(this, Observer {finalScore ->
+            binding.scoreText.text = finalScore.toString()
+        })
 
         return binding.root
     }
--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.android.guesstheword.screens.score
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class ScoreViewModel (finalScore:Int): ViewModel(){
 //class ScoreViewModel (private val finalScore:Int): ViewModel(){
-    val score = finalScore
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     init{
         Timber.i("init ScoreViewModel with $finalScore")
+        _score.value = finalScore
     }
 }
