1049cb79fbdc4877356f8a571d1f458b165a3270
Ed Johnson <ed@edwardotis.com>
2020-01-15 15:55:56 -0800

    refactor viewholder binding

--- RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -26,37 +26,7 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.SleepViewHolder
 
     override fun onBindViewHolder(holder: SleepViewHolder, position: Int) {
         val item = data[position]
-//        holder.quality.text = item.sleepQuality.toString()
-        val res = holder.itemView.context.resources//purely for helper function
-        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
-        if (item.sleepQuality <= 1) {
-            holder.quality.setTextColor(Color.RED) // red
-        }else{
-            holder.quality.setTextColor(Color.BLACK)
-        }
-        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-
-        //set image quality icon
-//        when(item.sleepQuality){
-//            0 -> holder.qualityImage.setImageResource(R.drawable.ic_sleep_0)
-//            1 -> holder.qualityImage.setImageResource(R.drawable.ic_sleep_1)
-//            2 -> holder.qualityImage.setImageResource(R.drawable.ic_sleep_2)
-//            3 -> holder.qualityImage.setImageResource(R.drawable.ic_sleep_3)
-//            4 -> holder.qualityImage.setImageResource(R.drawable.ic_sleep_4)
-//            4 -> holder.qualityImage.setImageResource(R.drawable.ic_sleep_5)
-//            else ->{
-//                holder.qualityImage.setImageResource(R.drawable.ic_sleep_active)
-//            }
-//        }
-        holder.qualityImage.setImageResource(when (item.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+        holder.bind(item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SleepViewHolder {
@@ -69,6 +39,26 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.SleepViewHolder
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources//purely for helper function
+            quality.text = convertNumericQualityToString(item.sleepQuality, res)
+            if (item.sleepQuality <= 1) {
+                quality.setTextColor(Color.RED) // red
+            } else {
+                quality.setTextColor(Color.BLACK)
+            }
+            sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            qualityImage.setImageResource(when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            })
+        }
     }
 
 }
