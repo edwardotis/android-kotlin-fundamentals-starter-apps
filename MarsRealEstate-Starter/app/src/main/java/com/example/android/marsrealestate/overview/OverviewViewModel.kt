d1194c16b2ae8d98e40f62bb94797ce4d233dc4d
Ed Johnson <ed@edwardotis.com>
2020-01-24 17:31:19 -0800

    functional binding

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -27,26 +27,26 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-
+enum class MarsApiStatus { LOADING, ERROR, DONE }
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<MarsApiStatus>
+        get() = _status
 
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(
             viewModelJob + Dispatchers.Main)
 
-    private val _property = MutableLiveData<MarsProperty>()
-    val property: LiveData<MarsProperty>
-        get() = _property
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -62,16 +62,19 @@ class OverviewViewModel : ViewModel() {
         coroutineScope.launch {
             var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
+                _status.value = MarsApiStatus.LOADING
                 var listResult = getPropertiesDeferred.await()
-                _response.value =
-                        "Success: ${listResult.size} Mars properties retrieved"
+                _status.value = MarsApiStatus.DONE
+//                        "Success: ${listResult.size} Mars properties retrieved"
                 if (listResult.isNotEmpty()) {
-                    _property.value = listResult.first()
+                    _properties.value = listResult
                 }
                 //ah, else required to have single line if statement, and we don't want to set it to null. kk.
 //                _property.value = if (listResult.isNotEmpty()) listResult.first() else null
             } catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = listOf()
+//                _status.value = "Failure: ${e.message}"
             }
 
         }
