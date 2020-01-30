91b133ba7dddc4fb58b3a0dc15af871052d79d0c
Ed Johnson <ed@edwardotis.com>
2020-01-29 20:49:08 -0800

    working from worker

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWorker.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWorker.kt
@@ -4,22 +4,44 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import retrofit2.HttpException
+import timber.log.Timber
 
 class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
         CoroutineWorker(appContext, params) {
+    /**
+     * This is the job for all coroutines started by this ViewModel.
+     *
+     * Cancelling this job will cancel all coroutines started by this ViewModel.
+     */
+    private val refreshDataWorkerJob = SupervisorJob()
+
+    /**
+     * This is the main scope for all coroutines launched by MainViewModel.
+     *
+     * Since we pass viewModelJob, you can cancel all coroutines launched by uiScope by calling
+     * viewModelJob.cancel()
+     */
+    private val refreshDataWorkerScope = CoroutineScope(refreshDataWorkerJob + Dispatchers.Main)
 
     override suspend fun doWork(): Result {
         val database = getDatabase(applicationContext)
         //TODO What's best way to handle CoroutineScope with BoundaryCallback
         //I guess not tieing it to a viewmodel apparently.
-//        val repository = VideosRepository(database)
-//        try {
-//            Timber.d("Work request for sync is run")
-//            repository.refreshVideos()
-//        } catch (e: HttpException) {
-//            Timber.w("Work request failures: ${e}")
-//            return Result.retry()
-//        }
+//        Need to figure out coroutinescope for Worker calls.
+//        Probably Worker should own the scope and cancel it when killed.
+        val repository = VideosRepository(database, refreshDataWorkerScope)
+        try {
+            Timber.d("Work request for sync is run")
+            repository.refreshVideos()
+        } catch (e: HttpException) {
+            Timber.w("Work request failures: ${e}")
+            return Result.retry()
+        }
         return Result.success()
     }
 
