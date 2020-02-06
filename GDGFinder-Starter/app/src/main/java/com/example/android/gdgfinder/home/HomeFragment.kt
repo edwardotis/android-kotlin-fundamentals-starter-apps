f2c859688a601b3d15426abc6ac451327512f7ce
Ed Johnson <ed@edwardotis.com>
2020-02-06 13:24:52 -0800

    now we have functional FAB button

--- GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -22,7 +22,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -39,7 +41,13 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-
+        binding.viewModel = viewModel
+        viewModel.navigateToSearch.observe(this, Observer { doNav ->
+            if(doNav == true){
+                this.findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToGdgListFragment())
+                viewModel.onNavigatedToSearch()
+            }
+        })
         return binding.root
     }
 }
